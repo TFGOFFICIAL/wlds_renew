f56796810072114222ff89a2c966820b5833d9bf
TFGOFFICIAL <thomgerber03@gmail.com>
2022-10-19 17:49:28 +0200

    V2.0.2

--- src/components/NewItems.js
+++ src/components/NewItems.js
@@ -10,14 +10,12 @@ const NewItems = () => {
     <section className='section relative overflow-hidden lg:min-h-[540px]'>
       <div className='container mx-auto'>
         <div className='flex flex-col lg:items-center lg:flex-row'>
-          {/* text */}
-          <div className='flex flex-col items-baseline gap-x-6 mb-6 lg:mb-0'>
-            <h2 className='title max-w-[245px] lg:mt-[30px] lg:mb-[90px]'>
+          <div className='flex flex-col items-baseline gap-x-6 mb-6 lg:mb-[90px]'>
+            <h2 className='title max-w-[245px] lg:mt-[30px] lg:mb-[30px]'>
               {t("FOLLOWUP_TITLE")}
             </h2>
-            <p className='subtitle max-w-[245px] lg:mb-12'>{t("FOLLOWUP_SUBTITLE")}</p>
+            <p className='subtitle max-w-[300px] lg:mb-12'>{t("FOLLOWUP_SUBTITLE")}</p>
           </div>
-          {/* product slider */}
           <div className='lg:max-w-[800px] xl:max-w-[990px] lg:absolute lg:-right-24'>
             <NewItemsSlider />
           </div>
--- src/components/Wwu.js
+++ src/components/Wwu.js
@@ -10,14 +10,11 @@ const Wwu = () => {
     <section className='section relative overflow-hidden lg:min-h-[540px]'>
         <div className='container mx-auto'>
             <div className='flex flex-col lg:flex-row'>
-                {/* text */}
-                <div className='flex flex-col items-baseline gap-x-6 mb-6 lg:mb-0'>
-                    <h2 className='title max-w-[280px] lg:mt-[30px] lg:mb-[90px]'>
-                    {t("WWU_TITLE")}
+                <div className='flex flex-col items-baseline gap-x-6 mb-6 lg:mb-[90px]'>
+                    <h2 className='title max-w-[300px] lg:mt-[30px] lg:mb-[30px]'>
+                        {t("WWU_TITLE")}
                     </h2>
-                    <p className='subtitle max-w-[280px]'>
-                        {t("WWU_SUBTITLE")}
-                    </p>
+                    <p className='subtitle max-w-[300px] lg:mb-12'>{t("WWU_SUBTITLE")}</p>
                 </div>
                 <div className='lg:max-w-[800px] xl:max-w-[990px] lg:absolute lg:-right-24'>
                     <WWUSlider />
