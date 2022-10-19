53d153f2547d0fac7776c97c32cbacbea59d3da8
TFGOFFICIAL <thomgerber03@gmail.com>
2022-10-19 13:56:00 +0200

    accept cookie

--- src/components/CookiePopUp.js
+++ src/components/CookiePopUp.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { useState } from 'react';
-import { cookies } from '../data';
+import CookieConsent from "react-cookie-consent";
+import { useTranslation } from 'react-i18next';
 
 export default function CookiePopUp() {
-    const [show, setShow] = useState(true);
-    const { image } = cookies;
+  const { t } = useTranslation()
   return (
-    <div className={show ? 'w-[95vw] shadow-2xl rounded-2xl bg-white z-30 fixed w-[300px] bottom-5 left-5' : 'hidden'}>
-        <img src={image.type} alt='' className='rounded-t-2xl' />
-        <div className='flex flex-col'>
-            <div className='w-full flex flex-row justify-center my-5'>                
-                <div className='font-bold text-xl'>We use cookies !</div>
-            </div>
-            <div className='w-full flex flex-row justify-end'>
-                <button onClick={() => {setShow(false);}} className='text-white shadow-xl bg-blue-800 rounded-xl px-5 py-1 mr-2 mb-2'>OK</button>
-            </div>
-        </div>
-    </div>
+    <CookieConsent location="bottom" cookieName="accepted" expires={1} overlay>
+        {t('COOKIE_TXT')}
+    </CookieConsent>
   )
 }
--- src/data.js
+++ src/data.js
@@ -3,7 +3,6 @@ import {
   IoLogoTwitter,
   IoLogoFacebook,
   IoLogoInstagram,
-  IoMdAddCircle,
   IoIosCheckmarkCircle,
   IoIosArrowRoundForward,
 } from 'react-icons/io';
@@ -16,16 +15,6 @@ import BedImg from './assets/img/bed.png';
 import CupboardImg from './assets/img/cupboard.png';
 import LightingImg from './assets/img/lighting.png';
 import Web from './assets/img/web.png';
-import Product1Img from './assets/img/products/product-1.png';
-import Product2Img from './assets/img/products/product-2.png';
-import Product3Img from './assets/img/products/product-3.png';
-import Product4Img from './assets/img/products/product-4.png';
-import Product5Img from './assets/img/products/product-5.png';
-import Product6Img from './assets/img/products/product-6.png';
-import Product7Img from './assets/img/products/product-7.png';
-import Product8Img from './assets/img/products/product-8.png';
-import Product9Img from './assets/img/products/product-9.png';
-import Product10Img from './assets/img/products/product-10.png';
 import TestimonialImg from './assets/img/testimonial.jpg';
 import Avatar1Img from './assets/img/avatar-1.png';
 import Avatar2Img from './assets/img/avatar-2.png';
@@ -35,7 +24,7 @@ import ReactJS from './assets/img/ReactJS.png';
 import TailWindCSS from './assets/img/TailWindCSS.png';
 import TailWindUI from './assets/img/TailWindUI.png';
 import TailWindUITemplates from './assets/img/TailWindUITemplates.png';
-import CookieImg from './assets/img/cookie.jpg';
+import CookieImg from './assets/img/cookie.png';
 
 export const cookies = {
   image : <CookieImg />
@@ -97,162 +86,6 @@ export const newInStore = {
     {
       image: <Web />,
     }
-  ],
-};
-
-export const products = {
-  title: 'All Products',
-  subtitle:
-    'The products we provide only for you as our service are selected from the best products with number 1 quality in the world',
-  pages: [
-    {
-      productList: [
-        {
-          image: <Product1Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Ceiling Light',
-          price: 75,
-          oldPrice: 82,
-        },
-        {
-          image: <Product2Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Wood Chair',
-          price: 50,
-          oldPrice: 70,
-        },
-        {
-          image: <Product3Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Papper Cupboard',
-          price: 105,
-          oldPrice: 120,
-        },
-        {
-          image: <Product4Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Ole Gundorse Spring',
-          price: 75,
-          oldPrice: 82,
-        },
-        {
-          image: <Product5Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Treos Seroes 911',
-          price: 200,
-          oldPrice: 210,
-        },
-        {
-          image: <Product6Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Multi bilderman slibber',
-          price: 45,
-          oldPrice: 50,
-        },
-        {
-          image: <Product7Img />,
-          icon: <IoMdAddCircle />,
-          name: 'XORA corner desk',
-          price: 320,
-          oldPrice: 325,
-        },
-        {
-          image: <Product8Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Black Forest Series Wood',
-          price: 225,
-          oldPrice: 240,
-        },
-        {
-          image: <Product9Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Papper Cupboard',
-          price: 105,
-          oldPrice: 120,
-        },
-        {
-          image: <Product10Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Ole Gundorse Spring',
-          price: 75,
-          oldPrice: 82,
-        },
-      ],
-    },
-    {
-      productList: [
-        {
-          image: <Product1Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Ceiling Light',
-          price: 75,
-          oldPrice: 82,
-        },
-        {
-          image: <Product2Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Wood Chair',
-          price: 50,
-          oldPrice: 70,
-        },
-        {
-          image: <Product3Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Papper Cupboard',
-          price: 105,
-          oldPrice: 120,
-        },
-        {
-          image: <Product4Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Ole Gundorse Spring',
-          price: 75,
-          oldPrice: 82,
-        },
-        {
-          image: <Product5Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Treos Seroes 911',
-          price: 200,
-          oldPrice: 210,
-        },
-        {
-          image: <Product6Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Multi bilderman slibber',
-          price: 45,
-          oldPrice: 50,
-        },
-        {
-          image: <Product7Img />,
-          icon: <IoMdAddCircle />,
-          name: 'XORA corner desk',
-          price: 320,
-          oldPrice: 325,
-        },
-        {
-          image: <Product8Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Black Forest Series Wood',
-          price: 225,
-          oldPrice: 240,
-        },
-        {
-          image: <Product9Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Papper Cupboard',
-          price: 105,
-          oldPrice: 120,
-        },
-        {
-          image: <Product10Img />,
-          icon: <IoMdAddCircle />,
-          name: 'Ole Gundorse Spring',
-          price: 75,
-          oldPrice: 82,
-        },
-      ],
-    },
   ],
 };
 
