84e75685e5ad8f29a6344c0552c37244f934a8a1
Logic Web Design <admin@logic-web-design.com>
2022-11-03 19:38:09 +0100

    added swiss made icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // import logo
 import Logo from '../assets/img/logo.png';
+import SwissMade from '../assets/img/swiss-made.png';
 
 // import data
 import { footer } from '../data';
@@ -11,9 +12,12 @@ const Footer = () => {
     <footer className='section bg-primary text-white py-10'>
       <div className='container mx-auto'>
         <div className='flex flex-col items-center lg:flex-row lg:justify-between border-b border-opacity-75 border-gray-700 pb-5 lg:pb-5 mb-5'>
-          <a className='mb-6 lg:mb-0' href='#'>
-            <img className='h-8 lg:h-10' src={Logo} alt='' />
-          </a>
+          <div className='flex flex-row justify-center items-center mb-5 lg:mb-0 gap-2'>
+            <a href='#'>
+              <img className='h-12 lg:h-17' src={Logo} alt='' />
+            </a>
+            <img className='h-11 lg:h-15 rounded-lg' alt='SwissMade' src={SwissMade} />
+          </div>          
           <div className='flex gap-x-4'>
             {footer.social.map((item, index) => {
               return (
